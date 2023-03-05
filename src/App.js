d2659cb789f2c6778588b8524953c3f4520c061f
Anuraj2601 <122327745+Anuraj2601@users.noreply.github.com>
2023-03-05 20:27:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,291 @@
+
+import './App.css';
+
+
+    function App() {
+      return (
+        <><div>
+          <nav class="navbar is-danger is-fixed-top" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+              <a class="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+              </a>
+    
+              <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </div>
+    
+            <div id="navbarBasicExample" class="navbar-menu">
+              <div class="navbar-start">
+                <a class="navbar-item" href="#intro">
+                  <span class="icon">
+                    <i class="fas fa-info"></i>
+                  </span>
+                  Introduction
+                </a>
+    
+                <a class="navbar-item" href="#edu">
+                  <span class="icon">
+                    <i class="fas fa-book"></i>
+                  </span>
+                  Education
+                </a>
+    
+    
+    
+    
+    
+                <a class="navbar-item" href="#project">
+                  <span class="icon">
+                    <i class="fas fa-chart-pie"></i>
+                  </span>
+                  <span>project</span>
+                </a>
+    
+                <a class="navbar-item" href="#contact">
+                  <span class="icon">
+                    <i class="fas fa-envelope"></i>
+                  </span>
+                  <span>Contact</span>
+                </a>
+    
+    
+    
+              </div>
+            </div>
+    
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <a class="button is-danger">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a class="button is-light">
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </nav>
+    
+        </div><section class="hero is-danger block py-6 custom-font">
+    
+            <div class="hero-body">
+              <div class="column is-4 mx-auto">
+                <div class="title-underline"></div>
+              </div>
+    
+              <div class="columns mx-0">
+                <figure data-aos="fade-down" class="image p-6 m-auto column is-5">
+                  <img class="is-rounded" width='480' height='480' src="IMG_20221115_094937.jpg" alt="" />
+    
+                </figure>
+                <div class="column is-5 m-auto p-6 ">
+                  <p data-aos="fade-up">
+    
+                    <h1 class="title is-1 ml-6 has-text-black">HI There, I'm Anuraj </h1>
+                    <h2 class="title is-5 ml-6 has-text-black"> Undergraduate  student</h2>
+                    <h2 class="title is-5 ml-6 has-text-black"> University of Colombo School of Computing</h2>
+                  </p>
+                </div>
+    
+              </div>
+            </div>
+          </section><div id="intro" class="pt-6"></div><section class="hero  has-background-danger-light block py-6 custom-font">
+            <div class="column is-4 mx-auto">
+              <h2 data-aos="fade-out" class="title has-text-centered is-uppercase">
+                <span class="icon">
+                  <i class="fas fa-info"></i>
+                </span>
+                Introduction
+              </h2>
+              <div class="title-underline"></div>
+            </div>
+            <div class="hero-body">
+              <div class="column is-4 mx-auto">
+    
+                <div class="title-underline"></div>
+              </div>
+              <div class="columns mx-0">
+    
+    
+                <div class="column is-5 m-auto p-6 ">
+                  <p data-aos="fade-up">
+                    <h2 class="title is-3 has-text-black">I’m a goal oriented and ambitious person. Highly motivated in web-
+                      Development and Database Management System courses. I
+                      am a good timekeeper, always willing to
+                      learn new skills. I can work well in a team
+                      environment or as an individual. I'm
+                      passionate about exploring new domains,
+                      where I can learn and overcome
+                      challenges. </h2><br />
+    
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section><div id="edu" class="pt-6"></div><section class="hero  has-background-danger-light block py-6 custom-font">
+            <div class="column is-4 mx-auto">
+              <h2 data-aos="fade-out" class="title has-text-centered is-uppercase">
+                <span class="icon">
+                  <i class="fas fa-book"></i>
+                </span>
+                Education
+              </h2>
+              <div class="title-underline"></div>
+            </div>
+            <div class="hero-body">
+              <div class="column is-4 mx-auto">
+    
+                <div class="title-underline"></div>
+              </div>
+              <div class="columns mx-0">
+    
+    
+                <div class="column is-5 m-auto p-6">
+                  <p data-aos="fade-up">
+                    <h2 class="title is-3 has-text-black">
+    
+    
+                      <div class="container">
+                        <div class="columns">
+                          <div class="column">
+                            <div class="box">
+                              <div class="content">
+                                <h4 class="title is-5">2022</h4>
+    
+                                Information Systems  (undergradute)
+                                University of Colombo School of Computing
+                              </div>
+                            </div>
+                          </div>
+                          <div class="column">
+                            <div class="box">
+                              <div class="content">
+                                <h4 class="title is-5">2020</h4>
+                                GCE Advanced Level Exam.
+    
+                                Jaffna Nelliady Central College
+    
+                                Combined mathematics – A
+    
+                                Chemistry – B
+    
+                                Physics – C
+    
+                                English - C
+    
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+    
+                        <div class="columns">
+                          <div class="column">
+                            <div class="box">
+                              <div class="content">
+                                <h4 class="title is-5">2017</h4>
+                                GCE Ordinary Level Exam.
+    
+                                Jaffna Nelliady Central College
+    
+                                (7A, 2B)
+    
+    
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </h2>
+                  </p>
+    
+                </div>
+              </div>
+            </div>
+          </section><div id="project" class="pt-4"></div><section class="hero  has-background-danger-light block py-6 custom-font">
+            <div class="column is-4 mx-auto">
+              <h2 data-aos="fade-out" class="title has-text-centered is-uppercase">
+                <span class="icon">
+                  <i class="fas fa-chart-pie"></i>
+                </span>
+                PROJECT
+              </h2>
+              <div class="title-underline"></div>
+            </div>
+            <div class="hero-body">
+              <div class="column is-4 mx-auto">
+    
+                <div class="title-underline"></div>
+              </div>
+              <div class="columns mx-0">
+                <div class="column is-5 m-auto p-6 ">
+                  <p data-aos="fade-up">
+                    <h2 class="title is-3 has-text-black">
+    
+                      <li>Creating a website</li>
+    
+    
+                      <li>Measuring the internet speed</li>
+    
+    
+                      <li>Build an online radio station</li> </h2><br />
+    
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section><div id="contact" class="pt-6"></div><section class="hero  has-background-danger-light block py-6 custom-font">
+            <div class="column is-4 mx-auto">
+              <h2 data-aos="fade-out" class="title has-text-centered is-uppercase">
+                <span class="icon">
+                  <i class="fas fa-envelope"></i>
+                </span>
+                CONTACT
+              </h2>
+              <div class="title-underline"></div>
+            </div>
+            <div class="hero-body">
+              <div class="column is-5 mx-auto">
+    
+                <div class="title-underline"></div>
+              </div>
+              <div class="columns mx-0">
+    
+    
+                <div class="column is-5 m-auto p-6 ">
+                  <p data-aos="fade-up">
+                    <h2 class="title is-3 has-text-black">
+    
+                      <span class="icon">
+                        <i class="fas fa-phone"></i>
+                      </span> <li> +94762144980</li><br /><br />
+    
+    
+                      <span class="icon">
+                        <i class="fab fa-linkedin"></i>
+                      </span><li>
+                        https://www.linkedin.com/in/anuraj-selvasothy-660565213/</li><br /><br />
+    
+                      <span class="icon">
+                        <i class="fas fa-envelope-square"></i>
+                      </span>
+                      <li> anuselva2601raj@gmail.com</li>
+                    </h2><br />
+    
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section></>
+      );
+    }
+    
+    export default App;
+    
+  
+
+
